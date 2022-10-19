75ead6bff7f9ba9956fda70928beef7daf18f302
João Silveira <joaos20014@gmail.com>
2022-10-19 13:34:25 +0200

    removed all .cpp

--- src/segment_manager.hpp
+++ src/segment_manager.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <array>
-#include <iostream>
 #include <mutex>
+#include <cmath>
 #include <vector>
 
 #include "segment.hpp"
@@ -10,13 +10,51 @@
 class SegmentAllocator
 {
 public:
-  SegmentAllocator(std::size_t size, std::size_t align);
+  SegmentAllocator(std::size_t size, std::size_t align)
+      : align(align), shift_offset(log2(align))
+  {
+    all_segments = std::make_unique<SharedSegment[]>(MAX_SEGMENTS);
+    available.reserve(MAX_SEGMENTS);
+    for (auto i = 1; i <= MAX_SEGMENTS; ++i)
+    {
+      available.push_back(MAX_SEGMENTS - i);
+    }
+    ObjectId dummy;
+    allocate(size, &dummy);
+  }
 
-  bool allocate(std::size_t size, ObjectId *addr);
-  void free(ObjectId addr);
+  bool allocate(std::size_t size, ObjectId *addr)
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    if (available.empty())
+    {
+      return false;
+    }
+    auto next = available.back();
+    available.pop_back();
+    all_segments[next].allocate(size, align);
 
-  Object &find(ObjectId addr);
-  SharedSegment &find_segment(ObjectId addr);
+    *addr = ObjectId{next, 1, 0};
+    return true;
+  }
+
+  void free(ObjectId addr)
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    all_segments[addr.segment].deallocate();
+    available.push_back(addr.segment);
+  }
+
+  inline Object &find(ObjectId addr)
+  {
+    const auto segm_offset = addr.offset >> shift_offset;
+    return find_segment(addr)[segm_offset];
+  }
+
+  inline SharedSegment &find_segment(ObjectId addr)
+  {
+    return all_segments[addr.segment];
+  }
 
   const SharedSegment &first_segment() const noexcept
   {
